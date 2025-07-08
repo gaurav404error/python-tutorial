da505afcb12714e6d955f6588fa8f80e39c31156
gaurav404error <singhvashistha7232gmail.com>
2025-07-08 14:27:49 +0530

    new concept added

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-print("gaurav")
+# print("gaurav")
+
+# input output operations
+# input("message or prompt")
+# print("statement", or variable, end='')
+
+# Python Enhancement Proposal 8 (PEP8)
+# snake_case => firstname X => first_name `_/
+# class StudentDetails
+# PI = 3.14
+# GRAVITY = 9.8
+# : -> next print and 4 spaces or 1 tab (indentation)
+age = 67
+
+if age >= 18 and age <= 26 :
+    print("He can vote!")
+elif age > 26 and age <= 60:
+    print("He is old")
+else:
+    print("He can't vote!")
+
+# PEP 484
+# Type annotations
+name : str = "India"
+age : int = 12
+salary : float = 1277777777.78
+
+
+# nums = [
+#     1,2,3,4,5,\
+#     3455,567
+# ]
