5ef0100bb36eb967dd356793156b146bad4f0f08
gaurav404error <singhvashistha7232gmail.com>
2025-07-09 15:02:26 +0530

    hi

--- app.py
+++ app.py
@@ -34,20 +34,34 @@ salary : float = 1277777777.78
 #print("Gaurav Arora")
 #print("Gaurav Arora")
 
-num=int(input("enter any number 1 to 7:"))
-if num ==1:
-    print("sunday")
-elif num ==2:
-    print("monday")
-elif num ==3:
-    print("tuesday")
-elif num ==4:
-    print("wednesday")
-elif num ==5:
-    print("thrusday")
-elif num ==6:
-    print("friday")
-elif num ==7:
-    print("saturday")
+# num=int(input("enter any number 1 to 7:"))
+# if num ==1:
+#     print("sunday")
+# elif num ==2:
+#     print("monday")
+# elif num ==3:
+#     print("tuesday")
+# elif num ==4:
+#     print("wednesday")
+# elif num ==5:
+#     print("thrusday")
+# elif num ==6:
+#     print("friday")
+# elif num ==7:
+#     print("saturday")
+# else:
+#     print("please enter number btw 1 to 7")
+
+yr= int(input("enter the year"))
+if yr %100 ==0:
+    if yr %400 ==0:
+        print("entered year is a leap year")
+    else:
+        print("entered year is not leap year")
 else:
-    print("please enter number btw 1 to 7")
+    if yr % 4 ==0:
+        print("entered year is a leap year")
+    else:
+        print ("entered year is not a leap year")
+    
+ 
