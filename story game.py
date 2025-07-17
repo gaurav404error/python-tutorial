56c0b66b8f6a3cb4d019c2b4140055cfd9e0bf5f
gaurav404error <singhvashistha7232gmail.com>
2025-07-17 11:59:36 +0530

    ghost story

---
+++ story game.py
@@ -0,0 +1,22 @@
+
+name = input("Type your name: ")
+print(f"Welcome {name.title()} to this adventure!")
+print("you hear a women crying near the village pond at night")
+print("what will you do?")
+print("1.go near the sound")
+print("2.go back to home")
+print("3.call your friend")
+choice=input("enter your choice ")
+if choice=="1":
+    print("a ghost appear and says thank you for finding me... now you're my slave!")
+elif choice=="2":
+    print(" you escape safely. but the sound follow you in your dream.")
+elif choice=="3":
+    print("your friend arrives with a pandit jii. the ghost disappear.you're safe now!")
+else:
+    print("you wakeup and your dream is over.")
+
+
+
+
+
