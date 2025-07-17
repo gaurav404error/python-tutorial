eb56eeda01a83d49d63dd663b8e8186555eb9ba6
gaurav404error <singhvashistha7232gmail.com>
2025-07-17 10:51:06 +0530

    hlo

---
+++ if-else.py
@@ -0,0 +1,45 @@
+# Check a no is positive or no using only if.
+# number = int(input("Enter any number: "))
+
+# if number > 0:
+#     print("Positive number!")
+
+
+# Check a no is positive or no using if else.
+# number = int(input("Enter any number: "))
+
+# if number > 0:
+#     print("Positive number")
+# else:
+#     print("Negative number")
+
+
+# Error code
+# IndentationError: expected an indented block after 'if'
+# x = 1
+# total = 0
+        
+# if x != 0:
+# total += x
+# print(total)
+
+
+# If Elif Else
+# first_number = int(input("Enter the first number: "))
+# second_number = int(input("Enter the second number: "))
+
+# choice = int(input("""
+# Choose your option:
+# 1-Add 2-Substract 3-MUltiply 4-divide or Quit
+# """))
+
+# if choice == 1:
+#     print(f"{first_number} + {second_number} = {first_number+second_number}")
+# elif choice == 2:
+#     print(f"{first_number} - {second_number} = {first_number-second_number}")
+# elif choice == 3:
+#     print(f"{first_number} X {second_number} = {first_number*second_number}")
+# elif choice == 4:
+#     print(f"{first_number} / {second_number} = {first_number/second_number}")
+# else:
+#     print("Wrong choice! Goodbye!! ")
