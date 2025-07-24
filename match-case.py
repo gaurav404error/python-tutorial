65d2b633b81c6829d2c19caec721b1ebda24f6b9
gaurav404error <singhvashistha7232gmail.com>
2025-07-24 11:01:46 +0530

    saiyaara tu mera nahi

---
+++ match-case.py
@@ -0,0 +1,41 @@
+# choice = int(input("Enter between 1 to 10: "))
+
+# match choice:
+#     case 1: 
+#         print("Today is monday!")
+#     case 2: 
+#         print("Today is tuesday!")
+#     case 3: 
+#         print("Today is wednesday!")
+#     case 4: 
+#         print("Today is thrusday!")
+#     case 5: 
+#         print("Today is friday!")
+#     case 6: 
+#         print("Today is saturday!")
+#     case 7: 
+#         print("Today is sunday!")
+#     case _:
+#         print("Invalid choice!")
+
+# If the choices are even, print namaskaram else if the choices are odd, jai shree ram. Default, wrong choice!
+
+# choice = 1
+
+# match choice:
+#     case 1 | 3 | 5 | 7 | 9:
+#         print("Jai Shree Ram")
+#     case 2 | 4 | 6 | 8 | 10:
+#         print("Namaskaram")
+#     case _:
+#         print("Wrong choice!")
+
+# month = 5
+# day = 4
+# match day:
+#   case 1 | 2 | 3 | 4 | 5 if month == 4:
+#     print("A weekday in April")
+#   case 1 | 2 | 3 | 4 | 5 if month == 5:
+#     print("A weekday in May")
+#   case _:
+#     print("No match")
