3f509d929b16843a4bf8cfa7f9d19fb63df29d68
gaurav404error <singhvashistha7232gmail.com>
2025-07-25 12:17:18 +0530

    loop question.py

--- for-loop.py
+++ for-loop.py
@@ -52,12 +52,127 @@
 # while True:
 #     print("Gaurav Singh")
 
-i = 1
-while i <= 10:
-    print("Hello, How are you? I am Gaurav Singh Vashistha!")
-    i = i + 5
+# i = 1
+# while i <= 10:
+#     print("Hello, How are you? I am Gaurav Singh Vashistha!")
+#     i = i + 5
 
 
-print(ord("A")) # A - 65
+# print(ord("A")) # A - 65
 
-print(chr(107))
+# print(chr(107))
+
+
+
+
+# for i in range(1, 11):
+#     print(i)
+
+
+# for char in "Python":
+#     print(char)
+
+
+# i = 2
+# while i <= 20:
+#     print(i)
+#     i += 2
+
+
+# i = 10
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+
+
+# for i in range(1, 6):
+#     print(i ** 2)
+
+
+# for _ in range(5):
+#     print("Hello")
+
+# for _ in range(7):
+#     print("gaurav singh vashistha")
+
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+
+# fruits= ["mango","strawberry","lichi"]
+# for name in fruits:
+#     print(name)
+
+
+
+# total = 0
+# for i in range(1, 101):
+#     total += i
+# print(total)
+
+
+# for i in range(1, 51):
+#     if i % 5 == 0:
+#         print(i)
+
+
+# for i in range (1,91):
+#     if i % 9 ==0:
+#         print(i)
+
+
+# countt=0
+# i=5
+# while i>0:
+#     countt+=1
+#     i-=1
+# print(countt)
+# print("liftoff") 
+
+
+
+# for i in range(1, 11):
+#     print(f"5 x {i} = {5 * i}")
+
+
+# for i in range(1, 5):
+#     print("*" * i)
+
+
+# for i in range(1, 4):
+#     for j in range(1, 11):
+#         print(f"{i} x {j} = {i * j}")
+#     print("-----")
+
+
+# for i in range(1, 6):
+#     if i == 3:
+#         continue
+#     print(i)
+
+
+
+# text = "gaurav"
+# for char in text:
+#     if char == 'e':
+#         break
+#     print(char)
+
+
+
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         print(f"{i} is Even")
+#     else:
+#         print(f"{i} is Odd")
+
+
+# while True:
+#     user_input = input("Enter something (type 'exit' to quit): ")
+#     if user_input.lower() == 'exit':
+#         break
+ 
