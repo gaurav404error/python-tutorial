a8bcff1e5cf3fe128aee65f20ce0db5a4e7d3037
gaurav404error <singhvashistha7232gmail.com>
2025-07-26 11:13:27 +0530

    functions notes added

---
+++ user-functions.py
@@ -0,0 +1,37 @@
+# Blank functions
+# def greet():
+#     pass 
+
+# def greet():
+#     ... 
+
+# def greet():
+#     print()
+
+
+# Void function
+# def greet():
+#     print("Namaskaram Gaurav ji!")
+
+# return function
+# def add():
+#     x = 1
+#     y = 1
+#     return x + y 
+
+# print(add())
+
+# greet()
+
+# Parameterized functions
+# def add(x, y):
+#     return x + y 
+
+# print(add(12, 19))
+
+# keyword based function
+def greet(first_name, last_name, age):
+    print(f"Namaskaram {first_name} {last_name}! I am {age} years old.")
+
+# greet(12, "Vashishta", "Gaurav")
+greet(age=1, last_name="Vashishta", first_name="Gaurav")
