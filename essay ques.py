3f6e3a51fba39da866cd17610f88ccc7087862a8
gaurav404error <singhvashistha7232gmail.com>
2025-07-30 11:30:05 +0530

    question.py

---
+++ essay ques.py
@@ -0,0 +1,37 @@
+sentence = """
+At the beginning of the 17th century, social manners, the cultivation of politeness, and the training of an accomplished gentleman became the theme of many essayists.
+This theme was first exploited by the Italian Baldassare Castiglione in his Il libro del cortegiano (1528; The Book of the Courtier).
+The influence of the essay and of genres allied to it, such as maxims, portraits, and sketches, proved second to none in molding the behavior of the cultured classes, first in Italy, then in France, and, through French influence, in most of Europe in the 17th century.
+Among those who pursued this theme was the 17th-century Spanish Jesuit Baltasar Gracián in his essays on the art of worldly wisdom.
+"""
+
+# text= "pursued!"
+# index= sentence.index("pursued")
+# print(index)
+
+
+# print("number of sentences:",sentence.count("."))
+
+# vowel= "AEIOUaeiou"
+# converted=(['*'if char in vowel else char for char in sentence])
+# print(converted)
+
+# print("lower.case:",sentence)
+# print("upper.case:",sentence)
+
+# print("swapcase:",sentence.swapcase())
+
+# original[i] if modified[i] == '*' else modified[i]
+# for i in range(len(modified))
+
+# print(restored) 
+
+
+
+
+import math
+def numerical_derivaties (f,x,h =0.0001):
+    args:
+        f(function):
+
+
--- loop-questions.py
+++ loop-questions.py
@@ -1,3 +1,63 @@
-for i in range(1, 6):
-    print(i)
 
+# Question 1:
+# for i in range(1, 6):
+#     print(i)
+
+# question 2
+
+# fruits=["apple","banana","orange"]
+# for fruit in fruits:
+#     print(fruit)
+
+# question 3
+
+# for i in range (2,11,2):
+#     print(i)
+
+#question 4
+# total=0
+# for i in range (1,6):
+#     total +=i
+#     print(total)
+
+# question 5
+
+# text = "hello"
+# for char in text:
+#     print(char)
+
+# question 6
+
+# for i in range (5,0,-1):
+#     print(i)
+
+# question 7
+# for i in range (3,13):
+#     if i%3==0:
+#         print(i)
+
+# question 8
+
+# items = [10, 20, 30, 40]
+# count = 0
+# for item in items:
+#     count += 1
+# print(count)
+
+
+# question 9
+
+# for i in range(1, 11):
+#     if i == 7:
+#         break
+#     print(i)
+    
+
+# lst = [1, 2]
+# for i in lst:
+#     for j in lst:
+#         print(f"({i}, {j})")
+
+
+
+r
