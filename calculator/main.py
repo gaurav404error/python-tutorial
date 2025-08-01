a5b9856f75d67641e90ad4d8b20166fd4c47aeed
gaurav404error <singhvashistha7232gmail.com>
2025-08-01 15:15:34 +0530

    added new notes

---
+++ calculator/main.py
@@ -0,0 +1,11 @@
+from operations import remainder
+
+# Function based programming
+def main():
+    x = int(input("Enter the 1st number: "))
+    y = int(input("Enter the 2nd number: "))
+
+    remainder(x,y)
+
+if __name__ == '__main__':
+    main()
---
+++ calculator/operations.py
@@ -0,0 +1,27 @@
+def add(x:int,y:int) -> None:
+    result = x + y
+    print(f"Sum of {x} + {y} = {result}")
+
+def subtract(x:int,y:int) -> None:
+    result = x - y
+    print(f"Difference between {x} - {y} = {result}")
+
+def multiply(x:int,y:int) -> None:
+    result = x * y
+    print(f"Multiplication of {x} X {y} = {result}")
+
+def decimal_division(x:int,y:int) -> None:
+    result = x / y
+    print(f"Divison of {x} / {y} = {result}")
+
+def quotient(x:int,y:int) -> None:
+    result = x // y
+    print(f"Quotient of {x} // {y} = {result}")
+
+def remainder(x:int,y:int) -> None:
+    result = x % y
+    print(f"Remainder of {x} % {y} = {result}")
+
+def square_of(x:int) -> None:
+    result = x ** 2
+    print(f"Square of {x} = {result}")
