5027de5f8bead19cb412d98ae5af09cd34c7416d
gaurav404error <singhvashistha7232gmail.com>
2025-08-19 20:27:43 +0530

    bank managemet

---
+++ Bank managment.py
@@ -0,0 +1,52 @@
+class Account:
+    def __init__(self, acc_no, name, balance=0):
+        self.acc_no = acc_no
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"₹{amount} deposited. New Balance: ₹{self.balance}")
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            print(f"₹{amount} withdrawn. Remaining Balance: ₹{self.balance}")
+        else:
+            print("Insufficient balance!")
+
+    def check_balance(self):
+        print(f"Account Balance: ₹{self.balance}")
+
+
+class Bank:
+    def __init__(self, name):
+        self.name = name
+        self.accounts = {}
+
+    def create_account(self, acc_no, name, balance=0):
+        if acc_no in self.accounts:
+            print("Account already exists!")
+        else:
+            self.accounts[acc_no] = Account(acc_no, name, balance)
+            print(f"Account created for {name} with A/C No: {acc_no}")
+
+    def get_account(self, acc_no):
+        return self.accounts.get(acc_no, None)
+
+
+
+bank = Bank("State Bank of Python")
+
+# Create Accounts
+bank.create_account(101, "gaurav", 5000)
+bank.create_account(102, "shivam", 3000)
+
+# Transactions
+acc1 = bank.get_account(101)
+acc1.deposit(20000)
+acc1.withdraw(15000)
+acc1.check_balance()
+
+acc2 = bank.get_account(102)
+acc2.withdraw(5000) 
---
+++ oops by apna college.py
@@ -0,0 +1,26 @@
+# class Student:
+#     Name = "gaurav kumar"
+
+# s1 = Student()
+# print (s1.Name)
+
+# s2 = Student()
+# print (s2.Name)
+
+
+# class Car:
+#     color = "red"
+#     brand = "bmw"
+
+# car1 = Car()
+# print (car1.color)
+
+
+
+
+
+
+
+
+
+
