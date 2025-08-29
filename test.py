4f55f95b9134393b6aa3d8ab71579b81af0113b5
gaurav404error <singhvashistha7232gmail.com>
2025-08-29 11:12:16 +0530

    list tupple

--- test.py
+++ test.py
@@ -1,50 +1,79 @@
-# nums = [1,2,3]
+# # nums = [1,2,3]
 
-# # nums.append(12)
+# # # nums.append(12)
+
+# # # print(nums)
+
+# # nums.extend([56,67,89,43,12])
 
 # # print(nums)
 
-# nums.extend([56,67,89,43,12])
+# # # # clear()
+# # # nums.clear()
+
+# # # print(nums)
+
+# # # del
+# # del nums[0:2]
+
+# # print(nums)
+
+
+# nums = [65,69,33]
+
+# # nums.append(101)
+
+# # print(nums)
+
+# nums=["Apple", "grapes","mango"]
+
+# nums.extend(["litchi","kiwi","carrot"])
 
 # print(nums)
 
-# # # clear()
+# nums.extend([1,2,3,4,5])
+# print(len(nums))
+
+
 # # nums.clear()
-
 # # print(nums)
 
-# # del
 # del nums[0:2]
-
 # print(nums)
 
+nums = [1,2,3,4,5,6,7,1,1]
 
-nums = [65,69,33]
-
-# nums.append(101)
+# insert(index,value)
+# nums.insert(3,44)
 
 # print(nums)
-
-nums=["Apple", "grapes","mango"]
-
-nums.extend(["litchi","kiwi","carrot"])
+# pop()
+# nums.pop(0)
+# nums.pop()
+# nums.pop()
 
 print(nums)
 
-nums.extend([1,2,3,4,5])
-print(len(nums))
-
-
-# nums.clear()
+# remove(value)
+# nums.remove(1)
 # print(nums)
 
-del nums[0:2]
+# sort()
+# nums.sort()
+# print(nums)
+
+# reverse()
+# nums.reverse()
+# print(nums)
+
+# sort(reverse=True)
+nums.sort(reverse=True)
 print(nums)
 
 
+# ()
+n = (1,2,3,1,1,1)
 
+print(n.count(1))
 
-
-
-
-
+print(n.index(3))
