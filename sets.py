a9d89ae0885d1676628c5f181746ca635a40aa13
gaurav404error <singhvashistha7232gmail.com>
2025-08-30 11:48:50 +0530

    set notes added

---
+++ sets.py
@@ -0,0 +1,73 @@
+# Set - an unordered pair/collection of numbers/values that are unique.
+# Jb hme bola jaaye ki list mein se duplicate numbers of remove krna hai. 
+
+# numbers = [1,1,1,1,1,2,3,4]
+
+# print(list(set(numbers)))
+
+# fruits = set()
+
+# # add() -> set mein values add krega
+# fruits.add("Apple")
+# fruits.add("Banana")
+# fruits.add("Kiwi")
+
+# print(fruits)
+
+# new_fruits = {"Apple", "Orange", "Banana"}
+
+# # union - |
+
+# print(fruits | new_fruits)
+
+# # intersection - &
+# print(fruits & new_fruits)
+
+
+# set_a.union(set_b)
+# scores = {100, 200, 300}
+
+# new_scores = {400, 500, 600, 200}
+
+# print(scores.union(new_scores))
+# print(new_scores.union(scores))
+
+# # set_a.intersection(set_b)
+# print(scores.intersection(new_scores))
+
+# clear()
+# scores.clear()
+# print(scores) # set() if blank/cleared
+
+# new_scores.clear()
+# print(new_scores) 
+
+# difference (-)
+set_a = {1,2,3,4} # 3
+set_b = {1,2,4,6}
+
+# print(set_a - set_b)
+
+# # set_a.difference(set_b)
+# print(set_a.difference(set_b))
+# print(set_b.difference(set_a))
+
+# # set_a.symmetric_difference(set_b)
+# # It returns only the values that are not common on both the sets. Values that are unique.
+
+# print(set_a.symmetric_difference(set_b)) 
+
+# remove() 
+# set_a.remove(3)
+
+# print(set_a)
+
+# pop() 
+# Set ke case, pop() will remove first item from the set.
+# Jbki list mein last item sbse pehle remove hota hai.
+print(set_a.pop())
+print(set_a.pop())
+print(set_a.pop())
+print(set_a.pop())
+print(set_a.pop())
+
