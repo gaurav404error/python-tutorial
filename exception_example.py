bcdef006919186276b429d57ae600994409a35af
gaurav404error <singhvashistha7232gmail.com>
2025-09-09 19:40:50 +0530

    dictionary module question

---
+++ exception_example.py
@@ -0,0 +1,39 @@
+# Error -> Abnormal termination of a program. 
+# Syntax error, Indentation error, 
+# name == "Gaurav"
+# print(name)
+
+
+# Code is correct but the logic is incorrect. 
+# try:
+#     number = 5
+#     res = number / 0 
+
+#     print(res)
+
+# except Exception as e:
+#     print(f"Error: {e}")
+
+# try:
+#     n = int(input("Enter a number: "))
+#     print(n**2)
+# except ValueError as e:
+#     print(e)
+
+# decimal -> (0 - 9) => base - 10
+# binary -> (0,1) base - 2
+# octal -> (0-7) base - 8
+# hexadecimal -> (0,1,2,3,4,5,6,7,8,9,A(10),B(11),C(12),D(13),E(14),F(15)) -> base - 16
+
+# names = {
+#     "abhi" : 10000000000000000,
+#     "gaurav" : 20000000000000000000
+# }
+
+# print(names["rohan"])
+
+# nums = [1,2,3,4,5]
+# print(nums[5])
+
+with open("abc.txt","r") as f:
+    print(f)
--- ict_.py
+++ ict_.py
@@ -31,36 +31,105 @@ names = dict()
 # print(student)
 
 
-basket ={
-    "fruit": "apple",
-    "quantity": "ten",
-    "quality": "good"
-
-}
-
-print(basket["fruit"])
-basket['fruit']= "mango"
-print(basket)
-
-basket ["price"]= "fifty" 
-print(basket)
-
-del basket ['price']
-print(basket)
-print(basket.get("production"))
-print(basket.get("production","not available")) 
-
-# my_dict = {}
-
-# student ={
+# basket ={
+#     "fruit": "apple",
+#     "quantity": "ten",
+#     "quality": "good"
 
 # }
 
-# items()
-print(basket.items())
+# print(basket["fruit"])
+# basket['fruit']= "mango"
+# print(basket)
 
-# keys()
-print(basket.keys())
+# basket ["price"]= "fifty" 
+# print(basket)
 
-# values()
-print(basket.values())
+# del basket ['price']
+# print(basket)
+# print(basket.get("production"))
+# print(basket.get("production","not available")) 
+
+# # my_dict = {}
+
+# # student ={
+
+# # }
+
+# # items()
+# print(basket.items())
+
+# # keys()
+# print(basket.keys())
+
+# # values()
+# print(basket.values())
+
+
+# my_dict={i:i**2 for  i in range (1,11)}
+# print("assignment 1 dictionary:", my_dict)
+
+# print("value of key 5:",my_dict[5])
+# print("keys of dictionary:",list(my_dict.keys()))
+
+# my_dict[11]=121
+# print ("after adding (11,121):",my_dict)
+# my_dict.pop(1)
+# print("after removing key 1:",my_dict)
+
+# for index, value in my_dict.items():
+#     print(f"{index}:{value}")
+
+# cube_dict = {i:i**3 for i in  range(1,11)}
+# print("cubes dictionary:",cube_dict)
+
+# data = {
+#   {'Name' : 'Abhi', 'Age' : 25, 'Country' : 'Japan'},
+#   {'Name' : 'Alya', 'Age' : 17, 'Country' : 'Japan'},
+#   {'Name' : 'Vaani', 'Age' : 25, 'Country' : 'India'}
+# }
+
+
+# student = {
+#     "name": "gaurav",
+#     "age": 18,
+#     "grades":{
+#         "math": 46,
+#         "physics": 83,
+#         "chemistry": 79,
+#         "english":90
+#     }
+# }
+# print(student)
+
+
+# d= {i:[i*j for j in range (1,6)]for i in range (1,6)}
+# print(d) 
+
+
+#Dictionary of Tuples.
+#key= first 5 positive integers.
+# Values - tuple containing (key,key^2)
+# d= {i:(i,i**2)for i in range(1,6)}
+# print(d)
+
+# # Dictionary and list conversion.
+# # Dictionary with first 5 positive integers and their squares.
+# d = {i:i**2 for i in range (1,8)}
+# #convert dictionary to a list of tuples.
+# lst=list(d.items())
+# print (lst)
+
+
+
+
+# #Dictionary Filtering
+# # First 10 positive integers and their squares
+# d={i:i**2 for i in range (1,13)}
+# #Keep only even keys
+# filtered_d = {k:v for k,v in d.items()if k%2 ==0}
+# print(filtered_d)
+
+
+
+
